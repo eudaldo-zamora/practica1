e0d32d1ada22e774d9ea20eaec4b99c7be314b8a
Luis Iñiguez <live33@outlook.es>
2022-04-23 09:16:02 -0500

    error submitted fixed

--- src/main/java/utm/edu/ec/practica1/entity/Product.java
+++ src/main/java/utm/edu/ec/practica1/entity/Product.java
@@ -16,7 +16,6 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
-
     private String name;
     private String description;
     private Double value;
