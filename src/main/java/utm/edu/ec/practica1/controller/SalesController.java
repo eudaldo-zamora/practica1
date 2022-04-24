4c0f2d2ad40a935ab57f8c70faf91bf1ea234053
Luis Iñiguez <live33@outlook.es>
2022-04-24 08:40:55 -0500

    Solución al inconveniente de nombres

--- src/main/java/utm/edu/ec/practica1/controller/SalesController.java
+++ src/main/java/utm/edu/ec/practica1/controller/SalesController.java
@@ -2,8 +2,13 @@ package utm.edu.ec.practica1.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import utm.edu.ec.practica1.entity.Sales;
 import utm.edu.ec.practica1.entity.Ventas;
 import utm.edu.ec.practica1.service.ISalesProviderService;
+import utm.edu.ec.practica1.service.ISalesService;
+
+import java.util.Date;
+import java.util.List;
 
 @RestController
 @RequestMapping(path="/sales")
@@ -11,43 +16,25 @@ import utm.edu.ec.practica1.service.ISalesProviderService;
 public class SalesController {
 
     @Autowired
-    private ISalesProviderService salesService;
+    private ISalesProviderService salesProviderService;
 
+    @Autowired
+    private ISalesService salesService;
 
     @PostMapping()
     public void saveSales(@RequestBody Ventas sales) {
+        salesProviderService.saveSales(sales);
+    }
+
+    @PostMapping()
+    public void saveSales(@RequestBody Sales sales) {
         salesService.saveSales(sales);
     }
 
+    @GetMapping()
+    public List<Sales> findSalesByFecha(@RequestParam Date fecha) {
+        return salesService.findSalesByFecha(fecha);
+    }
 
 
 }
-import utm.edu.ec.practica1.entity.Product;
-import utm.edu.ec.practica1.entity.Sales;
-import utm.edu.ec.practica1.service.IProductService;
-import utm.edu.ec.practica1.service.ISalesService;
-
-import java.util.Date;
-import java.util.List;
-import java.util.UUID;
-
-@RestController
-@RequestMapping(path="/sales")
-public class SalesController {
-        @Autowired
-        private ISalesService salesService;
-
-        @PostMapping()
-        public void saveSales(@RequestBody Sales sales) {
-            salesService.saveSales(sales);
-        }
-
-        @GetMapping()
-        public List<Sales> findSalesByFecha(@RequestParam Date fecha) {
-            return salesService.findSalesByFecha(fecha);
-        }
-
-
-
-
-    }
